ea9e69e79121a04dfcedd899c908e74d16880a10
Marta X. Moraga <martax13mh@gmail.com>
2023-03-01 20:12:17 +0200

    Setting mongoose and cors

--- server/app.js
+++ server/app.js
@@ -2,11 +2,37 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+var cors = require('cors');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// mongoose settings
+const mongoDB = "mongodb://127.0.0.1:27017/finalproject";
+//mongoose.set('strictQuery', false);
+mongoose.connect(mongoDB);
+mongoose.Promise = Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, "MongoDB connection error"));
+
+// cors configuration
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.resolve("..", "client", "build")));
+    app.get("/*", (req, res) =>
+        res.sendFile(path.resolve("..", "client", "build", "index.html"))
+    );
+} else if (process.env.NODE_ENV === "development") {
+    var corsOptions = {
+        origin: "http://localhost:3000",
+        optionsSuccessStatus: 200,
+    };
+    app.use(cors(corsOptions));
+
+}
 
 app.use(logger('dev'));
 app.use(express.json());
